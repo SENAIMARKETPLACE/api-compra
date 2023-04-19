aed7ac69c1658e4a0984f6048f0a03f940ae35e0
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-04-19 12:56:02 -0300

    Build: Building Entities

---
+++ src/main/java/br/com/senai/sollaris/domain/Pagamento.java
@@ -0,0 +1,28 @@
+package br.com.senai.sollaris.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pagamentos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class Pagamento {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private LocalDateTime processamento;
+}
