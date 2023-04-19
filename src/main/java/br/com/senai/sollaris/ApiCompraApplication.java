d2cf92ab9149784f218ad34e77e005daf264bacd
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-04-18 21:40:16 -0300

    Build: Initial Commit

---
+++ src/main/java/br/com/senai/sollaris/ApiCompraApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.sollaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCompraApplication.class, args);
+	}
+
+}
